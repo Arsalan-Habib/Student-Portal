106ac367d00a01f54684557a982e5b21f48e3409
Syed Arsalan Habib <arsalanhabib13@gmail.com>
2021-06-07 16:26:25 +0500

    Created the initial Student model

---
+++ backend/models/Student.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const studentSchema = mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true,
+    },
+    seatNumber: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    faculty: {
+        type: String,
+        default: "CS",
+    },
+    phoneNumber: {
+        type: String,
+    },
+    emailAddress: {
+        type: String,
+        unique: true,
+    },
+    shift: {
+        type: String,
+        default: "morning",
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+    },
+    courses: [],
+});
+
+studentSchema.index(
+    {
+        seatNumber: 1,
+        phoneNumber: 1,
+    },
+    { unique: true }
+);
+
+const Student = mongoose.model("Student", studentSchema);
+
+module.exports = {
+    Student,
+};
